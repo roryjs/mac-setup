7e9d2e986f559ef5d7e7469c0862753e01953105
Rory J Sanderson <roryjs@users.noreply.github.com>
2022-08-04 10:17:44 +0200

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,9 @@
+
+
+
+defaults write com.apple.dock autohide-delay -int 0
+defaults write com.apple.dock autohide-time-modifier -float 0.3
+killall dock
+
+# set tracking speed to 5 in settings
+defaults write .GlobalPreferences com.apple.mouse.scaling -1
